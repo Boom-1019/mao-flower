9a100495caae4e328c1b37b1759ad6542925580a
Boom-1019 <c910178@gmail.com>
2022-07-19 15:18:36 +0800

    0.0.2(20220719)手刻開始

---
+++ public/js/dist/index.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$('#banner').slick({
+  autoplay: true,
+  // dots: true,
+  infinite: true,
+  speed: 400,
+  fade: true,
+  cssEase: 'linear'
+});
---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+
+$('#banner').slick({
+    autoplay: true,
+    // dots: true,
+    infinite: true,
+    speed: 400,
+    fade: true,
+    cssEase: 'linear'
+  });

6766e78e959dcd31cf786448e4daca07a667de50
Boom-1019 <c910178@gmail.com>
2022-07-20 15:04:53 +0800

    0.0.3(20220720)內容區域

--- public/js/dist/index.dev.js
+++ public/js/dist/index.dev.js
@@ -4,7 +4,13 @@ $('#banner').slick({
   autoplay: true,
   // dots: true,
   infinite: true,
-  speed: 400,
+  speed: 1500,
   fade: true,
   cssEase: 'linear'
+});
+$('.list').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000
 });

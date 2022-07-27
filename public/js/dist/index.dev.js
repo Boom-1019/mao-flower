4625e39919b7ba04d68212c3916daf271c679e07
Boom-1019 <c910178@gmail.com>
2022-07-27 10:36:44 +0800

    0.0.5(20220727)修改

--- public/js/dist/index.dev.js
+++ public/js/dist/index.dev.js
@@ -7,12 +7,43 @@ $('#banner').slick({
   speed: 1500,
   fade: true,
   cssEase: 'linear'
-});
+}); // $('.list').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+// });
+
 $('.list').slick({
+  dots: true,
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 3000
+  autoplaySpeed: 2000,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // infinite: true,
+      dots: true
+    }
+  }, {
+    breakpoint: 800,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  }, {
+    breakpoint: 500,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
 }); // 捲軸事件
 // 定義捲軸高變數
 
--- public/js/index.js
+++ public/js/index.js
@@ -8,14 +8,49 @@ $('#banner').slick({
     cssEase: 'linear'
   });
 
+  // $('.list').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 3000,
+  // });
+
   $('.list').slick({
+    dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
-  
-
+          
 
 // 捲軸事件
 
